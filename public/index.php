e06ba959b686305ebc9eb2c2ebd78e4c4187556d
Onah Isaac <iumeha@yahoo.com>
2023-04-13 11:03:00 +0100

    message

--- public/index.php
+++ public/index.php
@@ -7,3 +7,6 @@ require "../app/core/init.php";
 $url = $_GET['url'] ?? 'home';
 $url = strtolower($url);
 $url = explode("/", $url);
+
+$page_name = trim($url[0]);
+$filename = "../app/pages/".$page_name.".php";
