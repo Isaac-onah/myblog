64334de3b953e2163d1276a741e1639ba39eefab
Onah Isaac <iumeha@yahoo.com>
2023-04-13 12:12:00 +0100

    message

--- public/index.php
+++ public/index.php
@@ -10,3 +10,13 @@ $url = explode("/", $url);
 
 $page_name = trim($url[0]);
 $filename = "../app/pages/".$page_name.".php";
+
+$PAGE = get_pagination_vars();
+
+if(file_exists($filename))
+{
+	require_once $filename;
+}else
+{
+	require_once "../app/pages/404.php";
+}
