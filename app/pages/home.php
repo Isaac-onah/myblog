c5cf5f5a66f95cf5656f08f5351f0b9ca8421409
Onah Isaac <iumeha@yahoo.com>
2023-04-14 09:30:00 +0100

    message

--- app/pages/home.php
+++ app/pages/home.php
@@ -29,7 +29,7 @@
         <div id="preloader">
             <div id="status">
                 <div class="logo">
-                    <img src="images/logo.png" height="20" class="d-block mx-auto" alt="">
+                    <img src="<?=ROOT?>/assets/images/logo.png" height="20" class="d-block mx-auto" alt="">
                 </div>
                 <div class="spinner">
                     <div class="bounce1"></div>
