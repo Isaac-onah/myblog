97ef80fd4a5e0868a835324a548388fecc1a164a
Aiyzihk <iumeha@yahoo.com>
2023-10-23 12:53:17 +0100

    message

--- app/pages/includes/slider.php
+++ app/pages/includes/slider.php
@@ -1,77 +1,112 @@
-<link href="<?= ROOT ?>/assets/css/sliderstyle.css" rel="stylesheet">
-
 <!--image slider start-->
-<section class="section">
-	<div class="containerzar">
-		<div id="slizar">
-			<div class="itemzar" style="background-image: url(1.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
+<section class="section pb-0">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-6">
+				<div class="row">
+					<div class="containerzar">
+						<div id="slizar">
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-1.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-2.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-3.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-4.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-1.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+							<div class="itemzar" style="background-image: url(<?= ROOT ?>/assets/images/img-3.jpg);">
+								<div class="contentzar">
+									<div class="namezar">LUNDEV</div>
+									<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+									<button>See more</button>
+								</div>
+							</div>
+						</div>
+						<div class="buttons">
+							<button id="prevzar"><i class="fa-solid fa-angle-left"></i></button>
+							<button id="nextzar"><i class="fa-solid fa-angle-right"></i></button>
+						</div>
+					</div>
+				</div><!--end row-->
+			</div><!--end col-->
+
+			<!-- START SIDEBAR -->
+			<div class="col-lg-4 col-md-6 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+				<div class="sidebar sticky-sidebar">
+					<!-- RECENT POST -->
+					<div class="widget">
+						<div class="p-4 rounded shadow">
+							<h6 class="widget-title font-weight-bold pt-2 pb-2 bg-light rounded text-center">Recent Post</h6>
+							<div class="mt-4">
+								<div class="clearfix d-flex align-items-center post-recent">
+									<div class="post-recent-thumb"> <a href="jvascript:void(0)"> <img alt="img" src="<?= ROOT ?>/assets/images/blog/07.jpg" class="img-fluid rounded"></a></div>
+									<div class="post-recent-content"><a href="jvascript:void(0)">Consultant Business</a><span class="text-muted">15th April, 2020</span></div>
+								</div>
+								<div class="clearfix d-flex align-items-center post-recent">
+									<div class="post-recent-thumb"> <a href="jvascript:void(0)"> <img alt="img" src="<?= ROOT ?>/assets/images/blog/08.jpg" class="img-fluid rounded"></a></div>
+									<div class="post-recent-content"><a href="jvascript:void(0)">Look On The Glorious Balance</a> <span class="text-muted">15th April, 2020</span></div>
+								</div>
+								<div class="clearfix d-flex align-items-center post-recent">
+									<div class="post-recent-thumb"> <a href="jvascript:void(0)"> <img alt="img" src="<?= ROOT ?>/assets/images/blog/01.jpg" class="img-fluid rounded"></a></div>
+									<div class="post-recent-content"><a href="jvascript:void(0)">Research Financial.</a> <span class="text-muted">15th April, 2020</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- RECENT POST -->
+
+					<!-- CATAGORIES -->
+
+					<!-- TAG CLOUDS -->
+					<div class="widget mt-4 pt-2 text-center">
+						<div class="p-4 rounded shadow">
+							<h6 class="widget-title font-weight-bold pt-2 pb-2 bg-light rounded">Tags Cloud</h6>
+							<div class="tagcloud mt-4">
+								<a href="jvascript:void(0)" class="rounded">Business</a>
+								<a href="jvascript:void(0)" class="rounded">Finance</a>
+								<a href="jvascript:void(0)" class="rounded">Marketing</a>
+								<a href="jvascript:void(0)" class="rounded">Fashion</a>
+								<a href="jvascript:void(0)" class="rounded">Bride</a>
+								<a href="jvascript:void(0)" class="rounded">Lifestyle</a>
+								<a href="jvascript:void(0)" class="rounded">Travel</a>
+								<a href="jvascript:void(0)" class="rounded">Beauty</a>
+								<a href="jvascript:void(0)" class="rounded">Video</a>
+								<a href="jvascript:void(0)" class="rounded">Audio</a>
+							</div>
+						</div>
+					</div>
+					<!-- TAG CLOUDS -->
 				</div>
-			</div>
-			<div class="itemzar" style="background-image: url(4.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
-				</div>
-			</div>
-			<div class="itemzar" style="background-image: url(3.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
-				</div>
-			</div>
-			<div class="itemzar" style="background-image: url(4.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
-				</div>
-			</div>
-			<div class="itemzar" style="background-image: url(5.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
-				</div>
-			</div>
-			<div class="itemzar" style="background-image: url(6.jpg);">
-				<div class="contentzar">
-					<div class="namezar">LUNDEV</div>
-					<div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-					<button>See more</button>
-				</div>
-			</div>
-		</div>
-		<div class="buttons">
-			<button id="prevzar"><i class="fa-solid fa-angle-left"></i></button>
-			<button id="nextzar"><i class="fa-solid fa-angle-right"></i></button>
-		</div>
-	</div>
+			</div><!--end col-->
+			<!-- END SIDEBAR -->
+		</div><!--end row-->
+	</div><!--end container-->
 </section>
 <!--image slider end-->
-
-<script>
-	document.getElementById('nextzar').onclick = function() {
-		let lists = document.querySelectorAll('.itemzar');
-		document.getElementById('slizar').appendChild(lists[0]);
-	}
-	document.getElementById('prevzar').onclick = function() {
-		let lists = document.querySelectorAll('.itemzar');
-		document.getElementById('slizar').prepend(lists[lists.length - 1]);
-	}
-
-	// Function to go to the next item
-	function nextItem() {
-		let lists = document.querySelectorAll('.itemzar');
-		document.getElementById('slizar').appendChild(lists[0]);
-	}
-
-	// Set up a timer to automatically go to the next item
-	const autoSlideInterval = 5000; // Adjust the interval as needed (in milliseconds)
-	let autoSlideTimer = setInterval(nextItem, autoSlideInterval);
-</script>
